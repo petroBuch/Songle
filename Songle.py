cb8296db1204e92cf302857966d2de870c099fcb
petroBuch <118077220+petroBuch@users.noreply.github.com>
2023-01-15 22:59:14 +0300

    Update Songle.py

--- Songle.py
+++ Songle.py
@@ -1,10 +1,13 @@
 import kivy
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.gridlayout import GridLayout
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.gridlayout import GridLayout
 from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.core.window import Window
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivymd.uix.card import MDCard
+from kivymd.uix.behaviors import FakeRectangularElevationBehavior
+from kivy.uix.image import Image
 Window.size = (360, 700)
 
 
@@ -17,7 +20,10 @@ class Songle(MDApp):
 		screen_manager.current = "home"
 		return screen_manager
 			
-
+class ShadowCard(FakeRectangularElevationBehavior, MDBoxLayout):
+	pass
+class ShadowImage(FakeRectangularElevationBehavior, Image):
+	pass
 
 if __name__ == '__main__':
 	Songle().run()
