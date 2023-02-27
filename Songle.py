938107f3ab632fce735f489ddd53b150f8e76894
petroBuch <118077220+petroBuch@users.noreply.github.com>
2023-02-27 08:35:59 +0300

    Update Songle.py

--- Songle.py
+++ Songle.py
@@ -1,5 +1,7 @@
 # C:\users/salop/documents/github/songle
 import kivy
+from kivy.properties import ObjectProperty
+from kivymd.uix.floatlayout import MDFloatLayout
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.gridlayout import GridLayout
 from kivymd.app import MDApp
@@ -8,6 +10,7 @@ from kivy.core.window import Window
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivymd.uix.behaviors import CommonElevationBehavior
 from kivy.uix.image import Image
+from kivymd.uix.label import MDLabel
 Window.size = (360, 700)
 
 
@@ -19,7 +22,33 @@ class Songle(MDApp):
 		screen_manager.add_widget(Builder.load_file("Home.kv"))
 		screen_manager.current = "home"
 		return screen_manager
-	
+	def on_start(self):
+		data = {
+			"1": "Рекомендации",
+			"2": "Новинки",
+			"3": "Ремиксы",
+			"4": "Relax & Chill",
+			"5": "Новые имена",
+			"6": "Исполнитель",
+		}
+		for num in data.keys():
+			PLFloatLayout = MDFloatLayout()
+			PLFloatLayout.add_widget(Image(source=f"Items/plimage{num}.png", pos_hint={"x": 0, "y": 0}))
+			PLFloatLayout.add_widget(
+				MDLabel(
+					text = data[num],
+					font_size = "16sp",
+					# size_hint = (None, None),
+					size = (130, 20),
+					pos = (10, 65),
+					color = "#E4E4E4"
+				)
+			)
+			# self.root.screens[2].ids.plimage.source = f"Items/plimage{num}.png"
+			# # self.root.screens[2].ids.pltext.text = data[num]
+			self.root.screens[2].ids.plgl.add_widget(PLFloatLayout)
+
+
 class ShadowBox(CommonElevationBehavior, MDBoxLayout):
 	pass		
 class ShadowCard(CommonElevationBehavior, MDBoxLayout):
